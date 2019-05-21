ae8638cd7c6360a4042e5848ff845a317412dec9
Heath JH Moore <heathjhmoore@gmail.com>
2019-05-20 19:54:58 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,12 @@
 import 'bootstrap';
 import '../styles/main.scss';
+import fb from 'firebase/app';
 import auth from './components/Auth/auth';
 import birfday from './components/Birthday/birthday';
+import apiKeys from './helpers/apiKeys.json';
 
 const init = () => {
+  fb.initializeApp(apiKeys.firebaseKeys);
   auth.authStringBuilder();
   birfday.birfdayStringBuilder();
 };
