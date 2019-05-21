4fa552e7e062de143338f41243927bdee5d69dfe
Heath JH Moore <heathjhmoore@gmail.com>
2019-05-20 21:26:46 -0500

    got auth working

--- src/javascripts/components/Auth/auth.js
+++ src/javascripts/components/Auth/auth.js
@@ -1,8 +1,18 @@
+import fb from 'firebase/app';
+import 'firebase/auth';
 import util from '../../helpers/util';
+import googleImage from './download.png';
+
+const signMeIn = () => {
+  const provider = new fb.auth.GoogleAuthProvider();
+  fb.auth().signInWithPopup(provider);
+};
 
 const authStringBuilder = () => {
-  const domString = 'Auth';
+  let domString = '<button id="google-auth" class="btn btn-danger p-0">';
+  domString += `<img src=${googleImage}/>`;
   util.printToDom('auth', domString);
+  document.getElementById('google-auth').addEventListener('click', signMeIn);
 };
 
 export default { authStringBuilder };
